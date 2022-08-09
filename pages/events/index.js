2638d32feb395cb5c9615cea34549f8df0f142f7
Sunset-kim <wood2002@naver.com>
2022-08-10 02:31:26 +0900

    filter -> page routing 기능 추가

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,20 @@
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../mock/data-dummy";
 
 function EventsPage() {
   const allEvents = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <div>
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={allEvents} />
     </div>
   );
