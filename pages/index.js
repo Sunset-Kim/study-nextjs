2578cacb4ac6a89d63b09208316b7ee6cec1b48c
Sunset-kim <wood2002@naver.com>
2022-08-09 21:19:34 +0900

    1.6 네비게이팅 : 정적라우터로 네비게이팅

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
+
+      <ul>
+        <li>
+          <Link href={"/portfolio"}>포트폴리오</Link>
+        </li>
+        <li>
+          <Link href={"/clients"}>클라이언트</Link>
+        </li>
+      </ul>
     </div>
   );
 }
