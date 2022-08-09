8bfd95dd15c12ef3ab81b2dd99e89dcd9ab0937b
Sunset-kim <wood2002@naver.com>
2022-08-10 02:06:17 +0900

    전체 이벤트를 렌더링

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,12 @@
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../mock/data-dummy";
+
 function EventsPage() {
+  const allEvents = getAllEvents();
+
   return (
     <div>
-      <h1>여기서 이벤트를 다 보여줍니다.</h1>
-
-      <div>여기를 누르면 디테일 페이지로 갑니다</div>
+      <EventList items={allEvents} />
     </div>
   );
 }
