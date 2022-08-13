397411b0b549c42567886a3bc0df06ac5d14a986
Sunset-kim <wood2002@naver.com>
2022-08-14 01:20:55 +0900

    client fetch - useSWR 사용해서 단계줄이기

--- pages/client-fetch/index.js
+++ pages/client-fetch/index.js
@@ -1,36 +1,51 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalePage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setLoading] = useState(false);
+  // const [sales, setSales] = useState();
+  // const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    fetch("https://nextjs-b302d-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const transformedSales = [];
+  const { data, error } = useSWR(
+    "https://nextjs-b302d-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
-        for (const key in data) {
-          transformedSales.push({ id: data[key].username, volume: data[key].volume });
-        }
-        setSales(transformedSales);
-        setLoading(false);
-      });
-  }, []);
+  const reformData =
+    data &&
+    Object.values(data).map((item) => {
+      console.log(item);
+      item["id"] = item.username;
+      return item;
+    });
 
-  if (isLoading) {
+  console.log(reformData);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch("https://nextjs-b302d-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({ id: data[key].username, volume: data[key].volume });
+  //       }
+  //       setSales(transformedSales);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  if (!data) {
     return <p>Loading....</p>;
   }
 
-  if (!sales) {
+  if (error) {
     return <p>Not yet...</p>;
   }
 
   return (
     <ul>
-      {sales?.map((item) => (
+      {reformData?.map((item) => (
         <li key={item.id}>
           {item.id} {item.volume}
         </li>
