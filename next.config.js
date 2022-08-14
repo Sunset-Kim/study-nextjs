43773b6495093890c3fc90c05529ebe718e439e5
Sunset-kim <wood2002@naver.com>
2022-08-15 03:13:52 +0900

    fix : config 변경

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    prependData: `
+    $primary: #ba3b46;
+$secondary: #61c9a8;
+$tertiary: #ed9b40;
+$white: #ffeedb;
+$brown: #aa8f66;`,
   },
 };
 
