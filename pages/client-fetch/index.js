450d7dbb99c1e9f9bb039cea7a1372343e0151f6
Sunset-kim <wood2002@naver.com>
2022-08-14 21:50:32 +0900

    client-fetch 와 static props 를 활용하기

--- pages/client-fetch/index.js
+++ pages/client-fetch/index.js
@@ -1,53 +1,34 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-function LastSalePage() {
-  // const [sales, setSales] = useState();
-  // const [isLoading, setLoading] = useState(false);
+function LastSalePage(props) {
+  const [sales, setSales] = useState(props.sales);
 
   const { data, error } = useSWR(
     "https://nextjs-b302d-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
     (url) => fetch(url).then((res) => res.json())
   );
 
-  const reformData =
-    data &&
-    Object.values(data).map((item) => {
-      console.log(item);
-      item["id"] = item.username;
-      return item;
-    });
-
-  console.log(reformData);
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetch("https://nextjs-b302d-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       const transformedSales = [];
-
-  //       for (const key in data) {
-  //         transformedSales.push({ id: data[key].username, volume: data[key].volume });
-  //       }
-  //       setSales(transformedSales);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  if (!data) {
-    return <p>Loading....</p>;
-  }
+  useEffect(() => {
+    if (data) {
+      const reformData = Object.values(data);
+      setSales(reformData);
+    }
+  }, [data]);
 
   if (error) {
-    return <p>Not yet...</p>;
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data && !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
     <ul>
-      {reformData?.map((item) => (
-        <li key={item.id}>
-          {item.id} {item.volume}
+      {sales?.map((item) => (
+        <li key={item.username}>
+          {item.username} {item.volume}
         </li>
       ))}
     </ul>
@@ -55,3 +36,18 @@ function LastSalePage() {
 }
 
 export default LastSalePage;
+
+export async function getStaticProps() {
+  const response = await fetch("https://nextjs-b302d-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json");
+  const data = await response.json();
+
+  let sales = [];
+
+  if (data) {
+    sales = Object.values(data);
+  }
+
+  return {
+    props: { sales },
+  };
+}
