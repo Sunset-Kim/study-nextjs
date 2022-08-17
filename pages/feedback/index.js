5c89314d9b9558a1785997b47f507820fc020600
Sunset-kim <wood2002@naver.com>
2022-08-18 08:01:40 +0900

    동적 API 다루기

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function Feedback(props) {
   const { feedbacks } = props;
+  const [selectedFeedback, setSelectedFeedback] = useState();
+
+  const loadFeedbackHandler = (id) => {
+    fetch(`/api/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSelectedFeedback(data.result);
+      });
+  };
 
   return (
-    <ul>
-      {feedbacks?.map((feedback) => (
-        <li key={feedback.id}>
-          {feedback.feedback}
-          <p>{feedback.email}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div>
+        <h2>Selected Feedback</h2>
+        {selectedFeedback?.email}
+      </div>
+      <ul>
+        {feedbacks?.map((feedback) => (
+          <li key={feedback.id}>
+            {feedback.feedback}
+            <p>{feedback.email}</p>
+            <button onClick={loadFeedbackHandler.bind(null, feedback.id)}>불러오기</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
